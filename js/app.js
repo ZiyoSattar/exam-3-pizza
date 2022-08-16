506a78dfc1dc68894b33c7769f25956f8816fe04
ZiyoSattar <muhhammadziyos@gmail.com>
2022-08-16 11:55:32 +0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 let elForm = document.querySelector(".body__form");
 let elResultDivCol = document.querySelector(".body__col-div");
-let elResultDivColChild = document.querySelector(".extra__div");
-let list1 = document.querySelector(".list1");
-let list1Content = document.querySelector(".list-div");
-let list2Content = document.querySelector(".list-div-2");
-let list3Content = document.querySelector(".list-div-3");
-let list4Content = document.querySelector(".list-div-4");
+let elResultDivColSub = document.querySelector(".extra__div");
+let elList1 = document.querySelector(".list1");
+let elList1Content = document.querySelector(".list-div");
+let elListContent = document.querySelector(".list-div-2");
+let elList3 = document.querySelector(".list-div-3");
+let elList4 = document.querySelector(".list-div-4");
 
 
-/* pizza thickness */
+/* pizza qalinligi */
 
 
 let elRadioLabel = document.querySelector(".radio-label1");
@@ -26,9 +26,9 @@ elRadio3.setAttribute("required", "required")
 elRadioLabel.setAttribute("class", "d-block ")
 elRadioLabel2.setAttribute("class", "d-block ")
 elRadioLabel3.setAttribute("class", "d-block ")
-elRadio.setAttribute("class", "form__radio-div-radio ms-3");
-elRadio2.setAttribute("class", "form__radio-div-radio ms-3");
-elRadio3.setAttribute("class", "form__radio-div-radio ms-3");
+elRadio.setAttribute("class", "form-check-input form__radio-div-radio ms-3");
+elRadio2.setAttribute("class", "form-check-input form__radio-div-radio ms-3");
+elRadio3.setAttribute("class", "form-check-input form__radio-div-radio ms-3");
 elRadio.setAttribute("id", "id1radio");
 elRadio2.setAttribute("id", "id2radio");
 elRadio3.setAttribute("id", "id3radio");
@@ -45,79 +45,90 @@ elRadio3.setAttribute("name", "qalinligi");
 elRadioLabel.appendChild(elRadio);
 elRadioLabel2.appendChild(elRadio2);
 elRadioLabel3.appendChild(elRadio3);
-/* pizza thickness */
+
+
+/* end */
 
 
 
-/* what to put on pizza */
+/* what to add to pizza */
+
+
 let arr = ["Pomidor", "Kurka goshti", " Zaytun", "Tuzlangan Bodring", "Qoziqorin", "Qazi"];
 for (let i = 0; i < arr.length; i++) {
 
   let elFormCheck = document.createElement("label");
   let elFormInput = document.createElement("input");
 
-  elFormInput.setAttribute("class", "input-check");
+  elFormInput.setAttribute("class", "form-check-input me-2 input-check");
   elFormInput.setAttribute("type", "checkbox");
-  elFormInput.setAttribute("name", "pitsani nima solinganligi");
+  elFormInput.setAttribute("name", "Pizzani nima solinganligi");
   elFormInput.value = arr[i];
   elFormCheck.textContent = arr[i];
-  list1.appendChild(elFormCheck);
+  elList1.appendChild(elFormCheck);
   elFormCheck.prepend(elFormInput);
 }
-/* what to put on pizza */
+/* end*/
 
 
-/* addition */
+/* extra souces */
+
+
 let elLabelExtraCheck1 = document.querySelector(".form__check-extra-div-label-1")
 let elLabelExtraCheck2 = document.querySelector(".form__check-extra-div-label-2")
 
 let elCheckboxExtra1 = document.createElement("input")
 let elCheckboxExtra2 = document.createElement("input")
 
-elCheckboxExtra1.setAttribute("class", "form__check-extra-div-checkbox ms-4 form-check-input");
-elCheckboxExtra2.setAttribute("class", "form__check-extra-div-checkbox ms-4 form-check-input");
+elCheckboxExtra1.setAttribute("class", " form__check-extra-div-checkbox ms-4 form-check-input");
+elCheckboxExtra2.setAttribute("class", " form__check-extra-div-checkbox ms-4 form-check-input");
 elCheckboxExtra1.setAttribute("type", "checkbox");
 elCheckboxExtra2.setAttribute("type", "checkbox");
 elCheckboxExtra1.setAttribute("value", "Achchiq");
 elCheckboxExtra2.setAttribute("value", "Sosiskali");
 elCheckboxExtra1.setAttribute("id", "extracheckbox1");
 elCheckboxExtra2.setAttribute("id", "extracheckbox2");
-elCheckboxExtra1.setAttribute("name", "Pitsa uchun qoshimcha");
-elCheckboxExtra2.setAttribute("name", "Pitsa uchun qoshimcha");
+elCheckboxExtra1.setAttribute("name", "Pizzaga qoshimcha");
+elCheckboxExtra2.setAttribute("name", "Pizzaga qoshimcha");
 
 elLabelExtraCheck1.appendChild(elCheckboxExtra1)
 elLabelExtraCheck2.appendChild(elCheckboxExtra2)
-/* addition */
+
+
+/* end */
 
 
 
-/* pizza thletickness */
+/* pizza selection */
+
+
 elSelect = document.querySelector(".form__select-div-select");
 let elSelectOption1 = document.createElement("option");
 elSelect.setAttribute("required", "required")
 elSelectOption1.setAttribute("class", "select__option")
 elSelectOption1.setAttribute("hidden", "hidden")
 elSelectOption1.setAttribute("selected", "selected")
-elSelectOption1.textContent = "tanlang"
+elSelectOption1.textContent = "Non turi"
 elSelect.appendChild(elSelectOption1);
-/* thickness of pizza bread */
 
 
-///arrays
-let selectArr = ["Yupqa", "Qalin", "O'rta"];
+/* end*/
+
+
+///arrays start
+
+
+let selectArr = ["Yupqa", "Qalin", "O'rta", "Extra qalin"];
 for(let i = 0; i < selectArr.length; i++) {
     let elOption = document.createElement("option");
     elOption.textContent= selectArr[i];
     elOption.value = (selectArr[i]);
     elSelect.appendChild(elOption);
-
-
-    
 }
 
 
 elSelect.addEventListener("change", function () {
-  list4Content[0].textContent = elOption.value;    
+  elList4[0].textContent = elOption.value;    
 })
 
 let formRadios = document.querySelectorAll(".form__radio-div-radio");
@@ -134,10 +145,10 @@ for (let elK = 0; elK < formRadios.length; elK++) {
 
       for (let j = 0; j < newExtraRadioArr.length; j++) {
         list3Item.textContent = newExtraRadioArr[j];
-        list3Content.appendChild(list3Item);
+        elList3.appendChild(list3Item);
       }
     } else {
-      list3Content.removeChild(list3Item);
+      elList3.removeChild(list3Item);
     }
   })
 
@@ -150,8 +161,8 @@ let formInputes = document.querySelectorAll(".input-check");
 let newArr = [];
 newArr.innerHtml = ""
 for (let el = 0; el < formInputes.length; el++) {
-  let list1Item = document.createElement("span");
-  list1Item.setAttribute("class", "d-inline-block me-3 mb-1 h-0 ")
+  let elList1Item = document.createElement("span");
+  elList1Item.setAttribute("class", "d-inline-block me-3 mb-1 h-0 ")
 
   formInputes[el].addEventListener("change", function () {
 
@@ -159,11 +170,11 @@ for (let el = 0; el < formInputes.length; el++) {
       newArr.push(formInputes[el].value)
 
       for (let i = 0; i < newArr.length; i++) {
-        list1Item.textContent = newArr[i];
-        list1Content.appendChild(list1Item)
+        elList1Item.textContent = newArr[i];
+        elList1Content.appendChild(elList1Item)
       }
     } else {
-      list1Content.removeChild(list1Item)
+      elList1Content.removeChild(elList1Item)
     }
   })
 
@@ -171,26 +182,26 @@ for (let el = 0; el < formInputes.length; el++) {
 
 
 
-let formExtraInputes = document.querySelectorAll(".form__check-extra-div-checkbox");
+let elFormExtra = document.querySelectorAll(".form__check-extra-div-checkbox");
 let newExtraArr = [];
 newExtraArr.innerHtml = ""
-for (let elI = 0; elI < formExtraInputes.length; elI++) {
+for (let elI = 0; elI < elFormExtra.length; elI++) {
   let list2Item = document.createElement("span");
   list2Item.setAttribute("class", "d-inline-block me-3 mb-1  ")
 
-  formExtraInputes[elI].addEventListener("change", function () {
+  elFormExtra[elI].addEventListener("change", function () {
 
-    if (formExtraInputes[elI].checked) {
-      newExtraArr.push(formExtraInputes[elI].value)
+    if (elFormExtra[elI].checked) {
+      newExtraArr.push(elFormExtra[elI].value)
 
       for (let k = 0; k < newExtraArr.length; k++) {
         list2Item.textContent = newExtraArr[k];
-        list2Content.appendChild(list2Item)
+        elListContent.appendChild(list2Item)
       }
     } else {
-      list2Content.removeChild(list2Item)
+      elListContent.removeChild(list2Item)
     }
   })
 
 }
-//arrays
+//arrays end
